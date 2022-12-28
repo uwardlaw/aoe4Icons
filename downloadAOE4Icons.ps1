a3a35d1eead5722a1b58da41af24d42a74921940
Uriah Wardlaw <Uriah.wardlaw@gmail.com>
2022-12-28 05:39:56 +0000

    Init add

---
+++ downloadAOE4Icons.ps1
@@ -0,0 +1,18 @@
+﻿
+$iconHtmlDir = $PSScriptRoot + "\buildMenus"
+Get-ChildItem -Path $iconHtmlDir |
+    Foreach-Object {
+        $(Get-Content $_.FullName) -split '"' | select-string -pattern 'img/' |
+            ForEach-Object{
+                $imgString.Add($_)
+            }
+    }
+
+$imgString | Sort-Object -Unique |
+    FoeachObject {
+        $uniqueImgString.Add(
+    }
+
+$null = New-Item  ".\icons" -Type Directory -Force -ea 0
+
+
